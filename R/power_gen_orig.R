8bd0015bdba318c4a02004d33c27f57fb8316b52
Sage <sage@ucsb.edu>
2023-03-14 16:43:25 -0700

    Connecting package to GitHub

--- R/power_gen_orig.R
+++ R/power_gen_orig.R
@@ -10,6 +10,7 @@
 #' @return power (W)
 #'
 # function definition
+
 power_gen_orig = function(height, flow, rho=1000, g=9.8, Keff=0.8) {
   result = rho * height * flow * g * Keff
   return(result)
